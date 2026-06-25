517240fc17186ffe494fe73162c6b237e74e40f9
kuang313 <kuangdx@outlook.com>
2026-06-25 15:14:31 +0800

    Add files via upload

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ import os
 class Config:
     GLM_API_KEY = os.environ.get('GLM_API_KEY', '')
     GLM_MODEL = os.environ.get('GLM_MODEL', 'glm-4-flash')
-    SECRET_KEY = os.environ.get('SECRET_KEY', os.urandom(24))
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'dev-secret')
